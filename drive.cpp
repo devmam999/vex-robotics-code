023c4a8e28dd9f280925ec0aceb3ae467144c992
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-02-24 18:26:42 -0800

    Update drive.cpp

--- drive.cpp
+++ drive.cpp
@@ -8,16 +8,16 @@ void setDrive(int left, int right) {
     driveRightFront = right;
 }
 
-//Driver Control Functions
-void setDriveMotors() {
+//Driver Control Functions (Don't need for now)
+/*void setDriveMotors() {
     int leftJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
     int rightJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
     if (abs(leftJoystick < 5)) 
         leftJoystick = 0;
     if (abs(rightJoystick < 5)) 
         rightJoystick = 0;
-    // setDrive(leftJoystick, rightJoystick);
-}
+    setDrive(leftJoystick, rightJoystick);
+}*/
 
 //Arcade Control Functions
 void setDriveArcade() {
@@ -25,5 +25,9 @@ void setDriveArcade() {
     int drive = controller.get_analog(ANALOG_LEFT_Y);
     int left = drive + turn;
     int right = drive - turn;
+    if (abs(turn) < 5)
+        turn = 0;
+    if (abs(drive) < 5)
+        drive = 0;
     setDrive(left, right);
 }
