ef2c3a5d0c3dcea89fb510849cfa77e6373d1b0d
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-02-24 19:29:18 -0800

    Update drive.hpp

--- drive.hpp
+++ drive.hpp
@@ -3,8 +3,8 @@
 //Helper Functions
 void setDrive(int left, int right);
 
-//Driver Control Function
-void setDriveMotors();
+//Driver Control Function (Don't need for now)
+// void setDriveMotors();
 
 //Arcade Control Function
 void setDriveArcade();
