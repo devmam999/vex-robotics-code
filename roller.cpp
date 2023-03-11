207d0448a90fb2a555adb820e42025139b5d433a
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-11 08:09:59 -0800

    Update roller.cpp

--- roller.cpp
+++ roller.cpp
@@ -8,7 +8,7 @@ void setRoller(int power) {
 //set roller control
 int rollerPower = 0;
 void setRollerMotors() {
-    if (controller.get_digital_press(pros::E_CONTROLLER_DIGITAL_L2)) 
+    if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) 
         rollerPower = 127;
     else {
         rollerPower = 0;
