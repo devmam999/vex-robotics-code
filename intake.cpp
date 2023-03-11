ad5ac02ea491717d26003ac9cdc5253947d280ab
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-10 21:36:03 -0800

    Update intake.cpp

--- intake.cpp
+++ intake.cpp
@@ -8,11 +8,13 @@ void setIntake(int power) {
 //get the intake controlled
 void setIntakeMotors() {
     int intakePower = 0;
+    int numPressed = 0;
     if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_Y)) {
-        intakePower = -127;
-    }
-    else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
-        intakePower = -127;
+        numPressed += 1;
+        if (numPressed % 2 == 1)
+            intakePower = 127;
+        else if (numPressed % 2 == 0)
+            intakePower = 0;
     }
     setIntake(intakePower);
 }
