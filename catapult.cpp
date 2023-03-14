0b006455fc262d1ee9d8a85da534c07f2795923e
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-13 19:27:42 -0700

    Update catapult.cpp

--- catapult.cpp
+++ catapult.cpp
@@ -8,19 +8,20 @@ void setCatapult(int power) {
 int catapultPower = 0;
 bool catapultOn = false;
 void setCatapultMotor() {
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_MASTER)) {
-        catapultOn = !catapultOn
-        if (catapultOn) 
+    if (pros::c::adi_digital_read(limitSwitch)) {
+        catapultOn = !catapultOn;
+        if (catapultOn) {
             catapultPower = 127;
+        }
         else {
             catapultPower = 0;
         }
     }
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
-        if (controller.get_digital(pros::E_CONTROLLER_MASTER))
-            catapultPower = 127;
+        if (!pros::c::adi_digital_read(limitSwitch))
+            catapultPower = 0;
         else {
-            cataputPower = 0;
+            catapultPower = 127;
         }
     }
     setCatapult(catapultPower);
