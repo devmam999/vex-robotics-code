ea244dfd680f7f57bbe7ef67b6d27ec690c915cf
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-15 22:33:22 -0700

    Update intakeOuttakeRoller.hpp

--- intakeOuttakeRoller.hpp
+++ intakeOuttakeRoller.hpp
@@ -1,11 +1,11 @@
 #include "main.h"
 
 //Intake
-void setIntake();
+void setIntake(int theIntakePower);
 //roller
-void setRoller();
+void setRoller(int theRollerPower);
 //outtake
-void setOuttake();
+void setOuttake(int theOuttakePower);
 //Set intake (outtake is inside intake)
 void setIntakeMotor();
 //Set roller
