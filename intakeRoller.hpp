e0228f066e2a225c906dc3bc11c785872f72286d
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-15 22:20:22 -0700

    Update intakeRoller.hpp

--- intakeRoller.hpp
+++ intakeRoller.hpp
@@ -4,7 +4,10 @@
 void setIntake();
 //roller
 void setRoller();
-//Set intake
+//outtake
+void setOuttake();
+//Set intake (outtake is inside intake)
 void setIntakeMotor();
 //Set roller
 void setRollerMotor();
+
