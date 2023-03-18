8a90fe75d6d9d56d406b02decc4f7ac64414a129
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-18 11:08:14 -0700

    Update catapult.cpp

--- catapult.cpp
+++ catapult.cpp
@@ -7,7 +7,6 @@ void setCatapult(int power) {
 
 //Setting up Catapult motor
 int catapultPower = 0;
-bool catapultOn = false;
 void setCatapultMotor() {
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
         if (limitSwitch.get_value()) 
