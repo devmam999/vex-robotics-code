26ccf1d3581c37f1c62fdebf02b386191e986cea
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-03-28 18:02:37 -0700

    Update globals.cpp

--- globals.cpp
+++ globals.cpp
@@ -7,17 +7,14 @@ pros::Motor driveLeft3(16, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER
 pros::Motor driveRight1(5, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor driveRight2(6, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor driveRight3(10, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
-//Intake port
-pros::Motor intake(9, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
-//Outtake port
-pros::Motor outtake(9, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_COUNTS);
-//Roller port
-pros::Motor roller(9, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
+//Intake and roller port
+pros::Motor rollerIntake(9, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 //Catapult port
 pros::Motor catapult(1, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_COUNTS);
 //Limit switch
-pros::ADIDigitalIn limitSwitch('1');
+pros::ADIDigitalIn limitSwitch('H');
 
 //Controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
 
