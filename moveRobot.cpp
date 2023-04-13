e77a44394f508916a9cfa70c47b24d6abd99070e
devmam999 <107886293+devmam999@users.noreply.github.com>
2023-04-12 18:40:10 -0700

    Update moveRobot.cpp

--- moveRobot.cpp
+++ moveRobot.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
 void moveRobot(int inches) {
-	driveLeft.move_relative(inches * 540 * M_PI_13, 600);
-	driveRight.move_relative(inches * 540 * M_PI_13, 600);
+	driveLeft.move_relative(inches * (540/13) * (1/M_PI), 600);
+	driveRight.move_relative(inches * (540/13) * (1/M_PI), 600);
 }
